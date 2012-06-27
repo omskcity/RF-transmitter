4370080d1c8cb737539f2153f6fc24fcf7d33c13
omskcity <morph@omskcity.net>
2012-06-27 12:46:32 +0700

    First push

---
+++ RF-Transmit-103/RF-Transmit-103.cob
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<Project>
+  <Device>
+    <Manufacturer id="ST" name="ST"/>
+    <Chip id="STM32F103VE" name="STM32F103VE"/>
+  </Device>
+  <Components>
+    <Component id="bf7c3c91-96ed-11df-80ae-001d7d723e56" name="CMSIS core"/>
+    <Component id="bfa102a1-96ed-11df-80ae-001d7d723e56" name="RCC"/>
+    <Component id="bfe42621-96ed-11df-80ae-001d7d723e56" name="GPIO"/>
+    <Component id="bff208d1-96ed-11df-80ae-001d7d723e56" name="EXTI"/>
+    <Component id="c3dde901-96ed-11df-80ae-001d7d723e56" name="USART"/>
+    <Component id="0e2e20b0-4bb6-11e0-8e4a-001d7dcd0baf" name="CMSIS Boot"/>
+    <Component id="64d2c4e0-4e11-11e0-8ec0-001d7dcd0baf" name="MISC"/>
+  </Components>
+</Project>
